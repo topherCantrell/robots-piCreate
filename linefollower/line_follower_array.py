98184ee067135c6c303874c8dbddfaf36c7371b2
Christopher Cantrell <topherCantrell@gmail.com>
2018-06-07 10:12:44 -0500

    work on the python

--- linefollower/line_follower_array.py
+++ linefollower/line_follower_array.py
@@ -1,7 +1,38 @@
 import smbus
 import time
-from sx1509_registers import REG_RESET, REG_INTERRUPT_MASK_A, REG_DIR_A, REG_DIR_B,\
-    REG_DATA_A, REG_DATA_B, REG_MISC, REG_CLOCK, REG_DEBOUNCE_CONFIG, REG_DEBOUNCE_ENABLE_B
+from sx1509_registers import REG_RESET, REG_INTERRUPT_MASK_A
+from sx1509_registers import REG_DIR_A, REG_DIR_B, REG_DATA_A, REG_DATA_B 
+from sx1509_registers import REG_MISC, REG_CLOCK, REG_DEBOUNCE_CONFIG, REG_DEBOUNCE_ENABLE_B
+    
+# TODO Interrupts
+
+'''
+
+From the schematics:
+https://cdn.sparkfun.com/datasheets/Sensors/Infrared/SparkFun%20Line%20Follower%20Array_v10.pdf
+
+Interrupt pin brought off board
+
+Data Port A:
+I/O[0] <- SEN0
+I/O[1] <- SEN1
+I/O[2] <- SEN2
+I/O[3] <- SEN3
+I/O[4] <- SEN4
+I/O[5] <- SEN5
+I/O[6] <- SEN6
+I/O[7] <- SEN7
+
+Data Port B:
+I/O[8] -> -IREN (enable all IR LEDs:0=ON, 1=OFF)
+I/O[9] -> -FBEN (enable feedback LEDs on top of the board:0=ON, 1=OFF)
+
+Oscillator is for IR LED brightness (PWM)
+
+Debounce allows any input to be debounced -- change is accepted only if the input value is identical at two consecutive sampling times
+
+'''
+
 
 class Follower:
     
@@ -15,9 +46,9 @@ class Follower:
         if test != 0xFF00:
             raise OSError('Could not talk to SX1509')
         
-        self._write_byte(REG_DIR_A,  0xFF)
-        self._write_byte(REG_DIR_B,  0xFC)
-        self._write_byte(REG_DATA_B, 0x01)
+        self._write_byte(REG_DIR_A,  0xFF) # All sensor lines are inputs
+        self._write_byte(REG_DIR_B,  0xFC) # Enable IR and Enable Feedback are outputs
+        self._write_byte(REG_DATA_B, 0x01) # IR LEDs off, Feedback on
         
         self._bar_strobe = False
         self._invert_bits = False
@@ -30,19 +61,19 @@ class Follower:
         
         # First make sure clock is configured
         temp = self._read_byte(REG_MISC)
-        if (temp & 0x70) == 0:
-            temp |= (1<<4) # Just default to no divider if not set
+        if (temp & 0x70) == 0:            
+            temp |= (1<<4) # ClkX = fOSC/(2^(1-1)) = fOSC/1 = fOSC (no divider)
             self._write_byte(REG_MISC, temp)            
+            
         temp = self._read_byte(REG_CLOCK)
         if (temp & 0x60) == 0:
             temp |= (1<<6) # Default to internal osc
             self._write_byte(REG_CLOCK,temp)
             
-        config_value &= 7 # 3-bit value
+        config_value &= 7 # 3-bit value. Higher = longer
         self._write_byte(REG_DEBOUNCE_CONFIG, config_value)
     
     def debounce_enabled(self,pin,is_enabled):
-        # TODO how about a disable?
         debounce_enable = self._read_word(REG_DEBOUNCE_ENABLE_B)
         if is_enabled:
             debounce_enable |= (1 << pin)
@@ -106,18 +137,52 @@ class Follower:
         return ret
     
     def get_position(self):
-        # TODO
-        pass
+        
+        # Assign values to each bit, -127 to 127, sum, and divide
+                
+        raw = self.get_raw()
+          
+        accumulator = 0
+        bits_counted = 0
+        
+        for i in range(7,3,-1):
+            if ((raw>>i)&1)==1:
+                accumulator += ((-32 * (i - 3)) + 1)
+                bits_counted += 1
+        for i in range(4):
+            if ((raw>>i)&1)==1:
+                accumulator += ((32 * (4 - i)) - 1)
+                bits_counted += 1
+                
+        if bits_counted>0:
+            return accumulator/bits_counted
+        else:
+            return 0  
     
     def get_density(self):
-        # TODO
-        pass
+        
+        raw = self.get_raw()        
+        bits_counted = 0
+        
+        for i in range(8):
+            if ((raw>>i)&1)==1:
+                bits_counted += 1
+                
+        return bits_counted
     
     def _read_byte(self,addr):
         return self._bus.read_byte_data(self._i2c_address,addr) 
     
     def _write_byte(self,addr,value):
-        self._bus.write_byte_data(self._i2c_address,addr,value)        
+        self._bus.write_byte_data(self._i2c_address,addr,value)    
+        
+    def _read_word(self,addr):
+        # TODO
+        pass
+    
+    def _write_word(self,addr,value):
+        # TODO
+        pass    
         
 follower = Follower()
 
