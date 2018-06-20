6921b1d681377c5296ce514e296b69b08b6f7e41
Christopher Cantrell <topherCantrell@gmail.com>
2018-06-20 09:54:52 -0500

    roomba became robot

--- challenges/SquareDance/solution/square_dance.py
+++ challenges/SquareDance/solution/square_dance.py
@@ -1,11 +1,11 @@
 import time
 import irobot_create
 
-# Connect to the roomba
-roomba = irobot_create.Create('/dev/ttyUSB0') # On the Raspberry Pi 
+# Connect to the create
+robot = irobot_create.Create('/dev/ttyUSB0') # On the Raspberry Pi 
     
 # We are driving, but stop automatically if we encounter problems
-roomba.set_mode_safe()    
+robot.set_mode_safe()    
 
 DRIVE_SPEED = 200
 DRIVE_TIME = 2
@@ -14,15 +14,15 @@ TURN_SPEED = 200
 TURN_TIME = 1.1
 
 for i in range(4):
-    roomba.set_drive_straight(DRIVE_SPEED)
+    robot.set_drive_straight(DRIVE_SPEED)
     time.sleep(DRIVE_TIME)
     
-    roomba.set_drive_spin_cw(TURN_SPEED)
+    robot.set_drive_spin_cw(TURN_SPEED)
     time.sleep(TURN_TIME)
     
-roomba.set_drive_stop() 
+robot.set_drive_stop() 
     
 # Graceful sleep (allows for charging)
-roomba.set_mode_passive()
-roomba.set_mode_stop()
+robot.set_mode_passive()
+robot.set_mode_stop()
     
