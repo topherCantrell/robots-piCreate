2d5b398187c4951517ad70dc55783f39e96330bb
tophe <tophe@DESKTOP-LBAREV3>
2019-07-30 21:48:17 -0500

    distribution

--- setup.py
+++ setup.py
@@ -1,27 +1,21 @@
-try:
-    # Try using ez_setup to install setuptools if not already installed.
-    from ez_setup import use_setuptools
-    use_setuptools()
-except ImportError:
-    # Ignore import error and assume Python 3 which already has setuptools.
-    pass
 
 from setuptools import setup, find_packages
 
-classifiers = ['Development Status :: Ongoing',
+setup(name              = 'irobotcreate',
+      version           = '2.1',
+      author            = 'Chris Cantrell',
+      author_email      = 'topherCantrell@gmail.com',
+      description       = 'Python code to control the irobotCreate2 from a raspberry pi',
+      license           = 'MIT',
+      classifiers       = [
                'Operating System :: POSIX :: Linux',
                'License :: OSI Approved :: MIT License',
                'Intended Audience :: Developers',
                'Programming Language :: Python :: 3',
                'Topic :: Software Development',
-               'Topic :: System :: Hardware']
-
-setup(name              = 'IRobotCreate',
-      version           = '2.0',
-      author            = 'Chris Cantrell',
-      author_email      = 'topherCantrell@gmail.com',
-      description       = 'Python code to control the irobotCreate2 from a raspberry pi',
-      license           = 'MIT',
-      classifiers       = classifiers,
-      url               = 'https://github.com/topherCantrell/robots-piCreate',      
+               'Topic :: System :: Hardware'],
+      url               = 'https://github.com/topherCantrell/robots-piCreate',  
+      install_requires  = [      
+        'serial'
+      ],    
       packages          = find_packages())
